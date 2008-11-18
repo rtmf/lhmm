5e4258019d0587fdf7254f141c7b2da0ffe52e75
Donny Viszneki <donny@teamspace.blackstreet.strangled.net>
2008-11-18 14:06:08 -0500

    fixed glut header include directives

--- src/game.cpp
+++ src/game.cpp
@@ -4,9 +4,11 @@
 #ifdef __MACH__
 #  include <OpenGL/gl.h>
 #  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
 #else
 #  include <GL/gl.h>
 #  include <GL/glu.h>
+#  include <GL/glut.h>
 #endif
 
 #include <math.h>
